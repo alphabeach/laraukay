1c67f13b0944b1f34a92c937db68b98507a5db6a
alphabeach <juryandrewnathaniel.lebosada@tup.edu.ph>
2024-07-16 13:47:42 +0800

    unang comm

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Storage;
 
 class ProductController extends Controller
 {
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
+            BrandsTableSeeder::class,
+            ProductBrandsTableSeeder::class,
         ]);
     }
 }
